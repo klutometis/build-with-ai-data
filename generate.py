b18d37d8b9cc712386c09acab8351b965d2df4b0
Peter Danenberg <pcd@roxygen.org>
2024-04-17 01:55:02 -0700

    Generate with dataframes.

--- generate.py
+++ generate.py
@@ -1,14 +1,12 @@
-import datetime
-import json
 import logging
 import os
-import pprint
-from dataclasses import asdict, dataclass
+from dataclasses import dataclass
+from datetime import date, datetime, timedelta
 from pathlib import Path
 
+import pandas as pd
 from absl import app
 from polygon import RESTClient
-from polygon.rest.models.indicators import IndicatorValue, MACDIndicatorValue
 
 import params
 
@@ -25,96 +23,125 @@ class Datum:
     rsi: float | None
 
 
-def get_news(now: str, ticker: str, polygon: RESTClient):
-    return [
-        news.title
-        for news in polygon.list_ticker_news(
-            ticker, limit=10, published_utc=now
+def date_from_utc(utc: str):
+    return datetime.fromisoformat(utc.rstrip("Z")).date().isoformat()
+
+
+def get_news(start: str, ticker: str, polygon: RESTClient):
+    return (
+        pd.DataFrame(
+            [
+                {"date": date_from_utc(news.published_utc), "news": news.title}
+                for news in polygon.list_ticker_news(
+                    ticker, published_utc_gt=start
+                )
+            ]
         )
-    ]
+        .set_index("date")
+        .groupby("date")["news"]
+        .agg(list)
+        .reset_index()
+        .set_index("date")
+    )
 
 
-def get_ema(now: str, ticker: str, polygon: RESTClient):
-    short, *rest = polygon.get_ema(
-        ticker=ticker,
-        timespan="day",
-        window=50,
-        series_type="close",
-        timestamp=now,
-    ).values or [IndicatorValue()]
-
-    long, *rest = polygon.get_ema(
-        ticker=ticker,
-        timespan="day",
-        window=200,
-        series_type="close",
-        timestamp=now,
-    ).values or [IndicatorValue()]
-
-    return (short.value, long.value)
+def date_from_milliseconds(milliseconds: int):
+    return datetime.fromtimestamp(milliseconds / 1000).date().isoformat()
 
 
-def get_macd(now: str, ticker: str, polygon: RESTClient):
-    macd, *rest = polygon.get_macd(
-        ticker=ticker,
-        timespan="day",
-        short_window=12,
-        long_window=26,
-        signal_window=9,
-        series_type="close",
-        timestamp=now,
-    ).values or [MACDIndicatorValue()]
+def get_ema(start: str, ticker: str, polygon: RESTClient):
+    short = pd.DataFrame(
+        [
+            {
+                "date": date_from_milliseconds(value.timestamp),
+                "ema_short": value.value,
+            }
+            for value in polygon.get_ema(
+                ticker=ticker,
+                timespan="day",
+                window=50,
+                series_type="close",
+                timestamp_gt=start,
+            ).values
+        ]
+    )
 
-    return (macd.value, macd.signal)
+    long = pd.DataFrame(
+        [
+            {
+                "date": date_from_milliseconds(value.timestamp),
+                "ema_long": value.value,
+            }
+            for value in polygon.get_ema(
+                ticker=ticker,
+                timespan="day",
+                window=200,
+                series_type="close",
+                timestamp_gt=start,
+            ).values
+        ]
+    )
+
+    return pd.merge(short, long, on="date").set_index("date")
 
 
-def get_rsi(now: str, ticker: str, polygon: RESTClient):
-    rsi, *rest = polygon.get_rsi(
-        ticker=ticker,
-        timespan="day",
-        window=14,
-        series_type="close",
-        timestamp=now,
-    ).values or [IndicatorValue()]
+def get_macd(start: str, ticker: str, polygon: RESTClient):
+    return pd.DataFrame(
+        [
+            {
+                "date": date_from_milliseconds(value.timestamp),
+                "value": value.value,
+                "signal": value.signal,
+            }
+            for value in polygon.get_macd(
+                ticker=ticker,
+                timespan="day",
+                short_window=12,
+                long_window=26,
+                signal_window=9,
+                series_type="close",
+                timestamp_gt=start,
+            ).values
+        ]
+    ).set_index("date")
 
-    return rsi.value
+
+def get_rsi(start: str, ticker: str, polygon: RESTClient):
+    return pd.DataFrame(
+        [
+            {
+                "date": date_from_milliseconds(value.timestamp),
+                "rsi": value.value,
+            }
+            for value in polygon.get_rsi(
+                ticker=ticker,
+                timespan="day",
+                window=14,
+                series_type="close",
+                timestamp_gt=start,
+            ).values
+        ]
+    ).set_index("date")
 
 
 def main(argv):
     logging.basicConfig(level=logging.INFO)
     polygon = RESTClient(params.POLYGON)
-    today = datetime.date.today()
-    one_year_ago = today - datetime.timedelta(days=365)
-    current_date = one_year_ago
+    start = date.today() - timedelta(days=365)
 
-    data = {}
-
-    while current_date <= today:
-        iso = current_date.isoformat()
-
-        ema_short, ema_long = get_ema(iso, TICKER, polygon)
-        macd_value, macd_signal = get_macd(iso, TICKER, polygon)
-
-        datum = Datum(
-            news=get_news(iso, TICKER, polygon),
-            ema_short=ema_short,
-            ema_long=ema_long,
-            macd_value=macd_value,
-            macd_signal=macd_signal,
-            rsi=get_rsi(iso, TICKER, polygon),
-        )
-
-        logging.info(pprint.pformat(datum))
-
-        data[iso] = asdict(datum)
-
-        current_date += datetime.timedelta(days=1)
+    data = get_news(start, TICKER, polygon).join(
+        [
+            get_ema(start, TICKER, polygon),
+            get_macd(start, TICKER, polygon),
+            get_rsi(start, TICKER, polygon),
+        ],
+        how="outer",
+    )
 
     with open(
         Path(os.environ["BUILD_WORKSPACE_DIRECTORY"]) / "data.json", "w"
     ) as f:
-        pprint.pp(data)
-        json.dump(data, f, indent=4)
+        data.to_json(f, orient="index", indent=4)
 
 
 if __name__ == "__main__":
